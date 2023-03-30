ff2a4822e8a115b8cc3abfb501750a4ee43eef66
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2023-03-30 19:40:06 -0400

    More Bullet stuf ig

--- Gun Game 2D/Assets/Scripts/Gun.cs
+++ Gun Game 2D/Assets/Scripts/Gun.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public Transform firePoint;
     public GameObject bulletPrefab;
-    public float bulletForce = 20f;
-    public float fireRate = 0.5f;
-    public float nextFireTime = 0f;
+    public float bulletVelocity = 10f;
+    public float bulletRange = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private Transform firePoint;
+
+    private void Start()
     {
-        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        firePoint = transform.Find("FirePoint");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
         {
-            nextFireTime = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
-    void Shoot()
+    private void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb2d = bullet.GetComponent<Rigidbody2D>();
-        rb2d.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.right * bulletVelocity;
+        Destroy(bullet, bulletRange);
     }
 }
