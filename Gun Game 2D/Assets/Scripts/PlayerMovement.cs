1cf5d754042bfd4d308eb3627e291eac9178cf60
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2023-03-30 17:04:31 -0400

    More Updates stuff
    Make game more

---
+++ Gun Game 2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    public float groundCheckDistance = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb2d;
+    private bool isGrounded = false;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0f);
+
+        rb2d.velocity = new Vector2(movement.x * speed, rb2d.velocity.y);
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+        else
+        {
+            Debug.Log("Not the floor");
+        }
+    }
+}
+
+
