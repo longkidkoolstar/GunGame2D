1f54cdd146f7e68d52114f3cdd2d01ec6157ac0f
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2023-03-30 19:53:41 -0400

    Added Bullet Range and Bullet Velocity

--- Gun Game 2D/Assets/Scripts/Gun.cs
+++ Gun Game 2D/Assets/Scripts/Gun.cs
@@ -4,30 +4,29 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public Transform firePoint;
     public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+    public float fireRate = 0.5f;
+    public float nextFireTime = 0f;
+    public float bulletRange = 10f;
     public float bulletVelocity = 10f;
-    public float bulletRange = 5f;
 
-    private Transform firePoint;
-
-    private void Start()
+    // Update is called once per frame
+    void Update()
     {
-        firePoint = transform.Find("FirePoint");
-    }
-
-    private void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
         {
+            nextFireTime = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
-    private void Shoot()
+    void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.velocity = firePoint.right * bulletVelocity;
+        Rigidbody2D rb2d = bullet.GetComponent<Rigidbody2D>();
+        rb2d.velocity = firePoint.right * bulletVelocity;
         Destroy(bullet, bulletRange);
     }
 }
