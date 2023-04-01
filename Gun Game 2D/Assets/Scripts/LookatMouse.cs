9d563193ff5b3577fecf47a1d7edd948d2e4522c
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2023-03-31 21:54:19 -0400

    Bugs fixes

---
+++ Gun Game 2D/Assets/Scripts/LookatMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookatMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = - (transform.position.x - Camera.mainCamera.transform.position.x);
+        Vector3 worldPos = Camera.mainCamera.ScreenToWorldPoint (mousePos);
+        ransform.LookAt (worldPos);    
+    }
+}
